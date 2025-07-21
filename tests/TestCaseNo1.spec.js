d27832db4be99b9e47c72f066c0778bd57ba5a3b
Haris Alvi <Haris.Alvi@SHMA.COM>
2025-07-21 15:05:27 +0500

    testcases given by saad

---
+++ tests/TestCaseNo1.spec.js
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://128.1.103.4/ver73_hico_ndb/');
+  await page.getByPlaceholder('Enter Distributor Id').click();
+  await page.getByPlaceholder('Enter Distributor Id').fill('000002');
+  await page.getByPlaceholder('Enter Distributor Id').press('Tab');
+  await page.getByPlaceholder('Enter User Id').fill('admin');
+  await page.getByPlaceholder('Enter User Id').press('Tab');
+  await page.getByPlaceholder('Enter User Password').fill('admin');
+  await page.getByRole('button', { name: 'Sign-In' }).click();
+});

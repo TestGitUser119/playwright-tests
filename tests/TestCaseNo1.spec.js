761da283837dbe10793caad768002e85da8626b3
Haris Alvi <Haris.Alvi@SHMA.COM>
2025-07-21 18:16:22 +0500

    TestCaseNo1 --Edit the DT Code

--- tests/TestCaseNo1.spec.js
+++ tests/TestCaseNo1.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('http://128.1.103.4/ver73_hico_ndb/');
   await page.getByPlaceholder('Enter Distributor Id').click();
-  await page.getByPlaceholder('Enter Distributor Id').fill('000002');
+  await page.getByPlaceholder('Enter Distributor Id').fill('000001');
   await page.getByPlaceholder('Enter Distributor Id').press('Tab');
   await page.getByPlaceholder('Enter User Id').fill('admin');
   await page.getByPlaceholder('Enter User Id').press('Tab');

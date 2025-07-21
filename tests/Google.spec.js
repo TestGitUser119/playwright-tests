208f1fc904757497975d54ae2196675699093c75
Haris Alvi <Haris.Alvi@SHMA.COM>
2025-07-21 19:01:09 +0500

    YML V1.8

--- tests/Google.spec.js
+++ tests/Google.spec.js
@@ -2,10 +2,4 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://google.com/');
-  await page.goto('https://www.google.com/?gws_rd=ssl');
-  await page.getByLabel('Search', { exact: true }).click();
-  await page.getByLabel('Search', { exact: true }).fill('wikipedia ');
-  await page.getByLabel('Search', { exact: true }).press('ArrowDown');
-  await page.getByLabel('Search', { exact: true }).press('ArrowDown');
-  await page.getByRole('link', { name: 'Pakistan Wikipedia https://en.wikipedia.org › wiki › Pakistan' }).click();
 });
